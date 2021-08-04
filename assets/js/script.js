366611ca6e150357e3b190f4ee85fc64bd35e2d9
Gui Barreto <barretogui90@gmail.com>
2021-08-03 21:31:41 -0300

    adicionando alert no script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const formCliente = document.forms.cadastroCliente;
 const formProduto = document.forms.cadastroProduto;
 
 
+
 formCliente.addEventListener('submit', (e) => {
         e.preventDefault();
     
@@ -10,15 +11,18 @@ formCliente.addEventListener('submit', (e) => {
         localStorage.setItem('cadastro-cliente', JSON.stringify({nome:nome.value, sobrenome:sobrenome.value, email:email.value, telefone:telefone.value})) 
     
         formCliente.reset()
-
-})
-
-formProduto.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const {nomeProduto, codigoProduto, quantidade} = formProduto; 
-    console.log(nomeProduto.value, codigoProduto.value, quantidade.value)
-    localStorage.setItem('cadastro-produto', JSON.stringify({nomeProduto:nomeProduto.value, codigoProduto:codigoProduto.value, quantidade:quantidade.value})) 
-
-    //formProduto.reset()
-})
+        window.alert('Cadastro de cliente efetuado'); 
+        
+    })
+    
+    formProduto.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        const {nomeProduto, codigoProduto, quantidade} = formProduto; 
+        console.log(nomeProduto.value, codigoProduto.value, quantidade.value)
+        localStorage.setItem('cadastro-produto', JSON.stringify({nomeProduto:nomeProduto.value, codigoProduto:codigoProduto.value, quantidade:quantidade.value})) 
+        
+        formProduto.reset()
+        window.alert('Cadastro de produto efetuado'); 
+    })
+    
